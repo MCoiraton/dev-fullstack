1a013a997894055456d336d24e20d21d03922182
dovagneau <mathis.coiraton@gmail.com>
2022-09-23 16:58:43 +0200

    first commit

---
+++ TD/covid-api/covid-api/src/main/java/org/polytech/covidapi/Admin.java
@@ -0,0 +1,7 @@
+package org.polytech.covidapi;
+
+public class Admin {
+    Integer id;
+    Centre centre;
+    
+}
---
+++ TD/covid-api/covid-api/src/main/java/org/polytech/covidapi/RendezVous.java
@@ -0,0 +1,14 @@
+package org.polytech.covidapi;
+
+import java.sql.Date;
+
+public class RendezVous {
+    private Integer id;
+    private String mail;
+    private Integer tel;
+    private String firstName;
+    private String lastName;
+    private Date appointmentDate;
+    
+
+}
