94fc678bbf45c38d64358545aba579fb230e5d5f
dovagneau <mathis.coiraton@gmail.com>
2022-09-30 15:37:24 +0200

    adding medecin touçatouça

--- TD/covid-api/covid-api/src/main/java/org/polytech/covidapi/Repository/MedecinRepository.java
+++ TD/covid-api/covid-api/src/main/java/org/polytech/covidapi/Repository/MedecinRepository.java
@@ -2,10 +2,11 @@ package org.polytech.covidapi.Repository;
 import java.util.List;
 
 import org.polytech.covidapi.Table.Medecin;
+import org.polytech.covidapi.Table.Centre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MedecinRepository extends JpaRepository <Medecin, Integer>{
-    List<Medecin> findByCentre (Integer centerId);
+    List<Medecin> findByCentre (Centre centre);
 }
---
+++ TD/covid-api/covid-api/src/main/java/org/polytech/covidapi/Rest/MedecinRest.java
@@ -0,0 +1,25 @@
+package org.polytech.covidapi.Rest;
+import java.util.List;
+
+import org.polytech.covidapi.Repository.MedecinRepository;
+import org.polytech.covidapi.Services.MedecinService;
+import org.polytech.covidapi.Table.Medecin;
+import org.polytech.covidapi.Table.Centre;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MedecinRest {
+    @Autowired
+    private MedecinService medecinService;
+
+    @GetMapping(path="/medecins")
+    public List<Medecin> getAll(){
+        return medecinService.getMedecins();
+    }
+    @GetMapping(path = "/centerMedecins")
+    public List<Medecin> getByCentre(Centre centerId){
+        return medecinService.getByCentre(centerId);
+    }    
+}
