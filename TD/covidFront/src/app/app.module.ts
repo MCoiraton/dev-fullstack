a17ca0e9c2a8d10c21780d4e962926ebbc022039
galaxycorn <juliann.gaillard@hotmail.fr>
2022-10-06 13:46:54 +0200

    Ajout liaison BDD

--- TD/covidFront/src/app/app.module.ts
+++ TD/covidFront/src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { CenterlistComponent } from './centerlist/centerlist.component';
+import { AppointmentComponent } from './appointment/appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchbarComponent,
+    CenterlistComponent,
+    AppointmentComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
