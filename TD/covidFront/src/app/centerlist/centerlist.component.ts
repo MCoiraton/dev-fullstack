c044ef76b46b93828cdec5c444817755eca58ec9
galaxycorn <juliann.gaillard@hotmail.fr>
2022-10-06 13:41:46 +0200

    Ajout des composants

---
+++ TD/covidFront/src/app/centerlist/centerlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-centerlist',
+  templateUrl: './centerlist.component.html',
+  styleUrls: ['./centerlist.component.css']
+})
+export class CenterlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
