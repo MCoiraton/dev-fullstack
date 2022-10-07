7586322f2ba1c07e8d1b97b8e74fc9a62aa62d81
dovagneau <mathis.coiraton@gmail.com>
2022-10-07 17:34:11 +0200

    mise a jour 2

--- TD/covidFront/src/app/appointment/appointment.service.ts
+++ TD/covidFront/src/app/appointment/appointment.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { HttpClient} from '@angular/common/http';
+import { Appointment } from "../interface/Appointment";
 
 @Injectable({
     providedIn:'root'//a changer
@@ -8,4 +9,7 @@ import { HttpClient} from '@angular/common/http';
 export class AppointmentService{
     constructor(private httpClient :HttpClient){}
     
+    createAppointment(appointment:Appointment):Observable<Appointment>{
+        return this.httpClient.post("")
+    }
 }
