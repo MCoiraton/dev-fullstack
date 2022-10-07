9ea810d812bf8f88b585717854f20f0c9ef13c2f
dovagneau <mathis.coiraton@gmail.com>
2022-10-07 16:20:34 +0200

    recup de la ville

--- TD/covidFront/src/app/centerlist/centerlist.component.ts
+++ TD/covidFront/src/app/centerlist/centerlist.component.ts
@@ -1,25 +1,34 @@
 import { HttpClient} from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs'
 import { CenterList } from '../interface/CenterList';
 
 @Component({
   selector: 'app-centerlist',
   templateUrl: './centerlist.component.html',
-  styleUrls: ['./centerlist.component.css']
+  styleUrls: ['./centerlist.component.css'],
 })
 export class CenterlistComponent implements OnInit {
+  
+  ville!: string;
+  private sub:any;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private route:ActivatedRoute) {
+   
+   }
   
   centers: CenterList = {
-    centerList : 
+    centerList : this.getAllCentersCity(this.ville);
   }
 
   getAllCentersCity(city : string) : Observable<CenterlistComponent[]>{
     return this.httpClient.get<CenterlistComponent[]>("api/centres")
   }
   ngOnInit(): void {
+    this.sub=this.route.params.subscribe(params=>{
+      this.ville=params['ville'];
+    });
   }
 
 }
