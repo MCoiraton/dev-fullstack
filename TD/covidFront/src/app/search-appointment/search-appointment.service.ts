41af9a96de47e056232d70eb5fae3421f50a3bca
Galaxycorn <juliann.gaillard@hotmail.fr>
2023-01-07 01:13:00 +0100

    Add appointment search with email

---
+++ TD/covidFront/src/app/search-appointment/search-appointment.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@angular/core";
+import { catchError, Observable, map, tap, of } from "rxjs";
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { SearchAppointmentComponent } from "./search-appointment.component";
+import { Center } from "../interface/Center";
+import { Appointment } from "../interface/Appointment";
+
+@Injectable({
+    providedIn:'root'//a changer
+})
+export class SearchAppointmentService{
+ 
+    private url = 'http://localhost:8080/rdvs'; //url pour le getRdvs
+    private urlSearch = 'http://localhost:8080/rdvs' //url pour le search qui permet d'ajouter le paramètre de la ville
+
+    constructor(
+        private http: HttpClient){ 
+        }       
+   
+    //On utilise un observable de notre interface pour pouvoir avoir un object qui prend en paramètres les variables de l'interface
+    getAppointment(): Observable<Appointment[]> {
+        return this.http.get<Appointment[]>(this.url)
+
+    }
+
+    //Pareil pour le search le premier if permet d'avoir un object nul vu qu'on refresh à chaque mise à jour du bloc recherche
+    searchAppointment(term: string): Observable<Appointment[]> {
+        if (!term.trim()) {
+            return of([]);
+        }
+        console.log(`${this.urlSearch}/${term}`)
+        return this.http.get<Appointment[]>(`${this.urlSearch}/${term}`)
+        
+    }
+    
+ 
+}
