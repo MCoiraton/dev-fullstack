108b8fbd2a38863e5214866a5b778d094254c0a6
Galaxycorn <juliann.gaillard@hotmail.fr>
2023-01-14 22:29:51 +0100

    Add getRole

--- TD/covid-api/covid-api/src/main/java/org/polytech/covidapi/Rest/Admin/SuperAdminRest.java
+++ TD/covid-api/covid-api/src/main/java/org/polytech/covidapi/Rest/Admin/SuperAdminRest.java
@@ -1,5 +1,14 @@
 package org.polytech.covidapi.Rest.Admin;
 
+import org.polytech.covidapi.Services.CentreService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(path = "superadmin")
 public class SuperAdminRest {
+    
+    //private final CentreService centreService;
     
 }
